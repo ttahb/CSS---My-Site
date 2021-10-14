6efc7bdbf1a02457543ea8ec0df3a9d7f6120a11
ttahb <bhattvijay69@hotmail.com>
2021-10-15 02:46:14 +0530

    added completed version 1.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-function modify_zindex(){
-  var x = document.getElementById("navbar");
-  x.style.zIndex = 0;
-}
+/*function modify_zindex(){
+  let x = document.getElementById("navbar");
+  let selection  = x[0].innerHTML
+  //x.style.zIndex = 0;
+  if(selection === "About"){
+    x.style.zIndex = 1;
+  } else if (selection === "Work") {
+    x.style.zIndex = -1;
+  } else if (selection === "Career") {
+    x.style.zIndex = -1;
+  }
+
+}*/
